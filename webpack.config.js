3d16109b7e12837eba55497fae78f49f8c7cf65d
Ford Hurley <ford.hurley@gmail.com>
2017-08-30 09:34:29 -0400

    Minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require("path");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "shader-canvas.js",
+    filename: "shader-canvas.min.js",
     path: path.resolve(__dirname, "build"),
     library: "ShaderCanvas",
   },
+  plugins: [
+    new UglifyJSPlugin(),
+  ],
 };
