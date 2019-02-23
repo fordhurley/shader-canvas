b408dd87318c5ee2974e5ccadbf95a56ef5c650a
Ford Hurley <ford.hurley@gmail.com>
2019-02-23 09:34:02 -0500

    Clamp to edge, to support non-power-of-two by default

--- src/shader-canvas.ts
+++ src/shader-canvas.ts
@@ -136,6 +136,8 @@ export class ShaderCanvas {
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 
     const location = gl.getUniformLocation(this.shaderProgram, name);
